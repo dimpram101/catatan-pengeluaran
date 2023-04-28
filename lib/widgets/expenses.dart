e9871ab3b5303a02b92b029f9b5d3121c7d268a4
dimpram101 <dimas2004211@gmail.com>
2023-04-28 16:43:20 +0800

    using MediaQuery

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -68,6 +68,9 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     Widget mainContent = const Center(
       child: Text("Tidak ada data!"),
     );
@@ -95,14 +98,25 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
